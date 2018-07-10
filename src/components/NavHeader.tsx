3b5218a67d735cfeb4ecee9455ca22ad07431cb9
mt_coff <mt.coff.00@gmail.com>
2018-07-10 21:43:59 +0900

    Add Weight Recorder class
    * add moment.js
    
    * fix NavHeader
    
    * add WeightRecorder class

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 interface Iprops {
   title: string;
@@ -15,6 +16,9 @@ export default class NavHeader extends React.Component<Iprops> {
       <div>
         <AppBar position="static">
           <Toolbar>
+            <IconButton color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
             <Typography variant="title" color="inherit">
               {this.props.title}
             </Typography>
--- tests/unit/specs/components/NavHeader.spec.tsx
+++ tests/unit/specs/components/NavHeader.spec.tsx
@@ -2,13 +2,16 @@ import { shallow } from "enzyme";
 import * as React from "react";
 
 import NavHeader from "@/components/NavHeader";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 /* tslint:disable:jsx-key */
 describe("NavHeader", () => {
-  it("AppBar, Toolbar, Typography should exist", () => {
+  it("AppBar, Toolbar, Typography, IconButton, MenuIcon should exist", () => {
     const wrapper = shallow(<NavHeader title={""} />);
     expect(wrapper.find(AppBar).length).toBe(1);
+    expect(wrapper.find(IconButton).length).toBe(1);
+    expect(wrapper.find(MenuIcon).length).toBe(1);
     expect(wrapper.find(Toolbar).length).toBe(1);
     expect(wrapper.find(Typography).length).toBe(1);
   });
