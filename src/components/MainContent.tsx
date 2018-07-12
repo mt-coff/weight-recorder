fcfd1778d8f7d08537ae28c03d027b960ffa1d5c
mt-coff <mt.coff.00@gmail.com>
2018-07-12 16:01:09 +0900

    add routing component

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
 
+import Routes from "@/Routes";
+import { BrowserRouter } from "react-router-dom";
+
 export default class MainContent extends React.Component {
   public render() {
-    return <div />;
+    return (
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    );
   }
 }
